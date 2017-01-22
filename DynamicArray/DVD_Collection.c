b0b3781394f6a4f1889ee26705ffa3135a65cce8
CaptainDaVinci <yashkothari199767@gmail.com>
2017-01-22 19:02:24 +0530

    Better formatting and tabular display, also added a switch statement for type of sorting

--- DynamicArray/DVD_Collection.c
+++ DynamicArray/DVD_Collection.c
@@ -18,14 +18,16 @@ typedef struct{
 } DVD;
 
 
-void displayDVD(DVD input);
-
+void displayDVD(DVD *input, int amount);
+void softWrap(char *title);
 int compareTitle(const void * input1, const void * input2);
 int compareMinutes(const void * input1, const void * input2);
 int comparePrice(const void * input1, const void * input2);
 
 int main()
 {
+
+	int option;
 
 	DVD collection[10] = {{117, 9.99, "Back to the Future"}, {176, 9.99, "Indiana Jones and the Raiders of the Lost Ark"},
 			{82, 6.99, "Army of Darkness"}, {85, 6.99, "Evil Dead 2"},
@@ -37,47 +39,40 @@ int main()
 			{90, 19.99, "Heavy Metal"}
 	};
 
-	// Display all DVDs
-	for(int i = 0; i < 10; i++)
+	printf("1. Show my collection\n");
+	printf("2. Sort by Duration\n");
+	printf("3. Sort by price\n");
+	printf("4. Sort by title\n");
+
+	scanf("%d", &option);
+
+	switch(option)
 	{
-		displayDVD(collection[i]);
+				// Display all DVDs.
+		case 1:	displayDVD(collection, 10);
+					break;
+
+				// sort by duration.
+		case 2: qsort(collection, 10, sizeof(DVD), compareMinutes);
+				displayDVD(collection, 10);
+				break;
+
+				// sort by price.
+		case 3: qsort(collection, 10, sizeof(DVD), comparePrice);
+				displayDVD(collection, 10);
+				break;
+
+				// sort by title.
+		case 4: qsort(collection, 10, sizeof(DVD), compareTitle);
+				displayDVD(collection, 10);
+				break;
+
 	}
 
-	// Display a Title of one, price of another, runtime of another.
 
-	printf("%s\n", collection[2].title);
-	printf("%i\n", collection[5].minutes);
-	printf("%.2f\n", collection[8].price);
-
-
-
-
-	qsort(collection, 10, sizeof(DVD), compareTitle);
-	printf("Title Sort \n");
-	for(int i = 0; i < 10; i++)
-	{
-		displayDVD(collection[i]);
-	}
-
-
-
-	qsort(collection, 10, sizeof(DVD), compareMinutes);
-	printf("Minute sort \n");
-	for(int i = 0; i < 10; i++)
-	{
-		displayDVD(collection[i]);
-	}
-
-
-
-
-	qsort(collection, 10, sizeof(DVD), comparePrice);
-	printf("Price sort \n");
-	for(int i = 0; i < 10; i++)
-	{
-		displayDVD(collection[i]);
-	}
-	return 0;
+	// printf("%s\n", collection[2].title);
+	// printf("%i\n", collection[5].minutes);
+	// printf("%.2f\n", collection[8].price);
 }
 
 int compareTitle(const void * input1, const void * input2)
@@ -145,7 +140,68 @@ int comparePrice(const void * input1, const void * input2)
 }
 
 
-void displayDVD(DVD input)
+void displayDVD(DVD *input, int amount)
 {
-	printf("%s, Runtime: %i, Price: %.2f\n", input.title, input.minutes, input.price);
+	printf("------------------------------------------\n");
+	printf("TITLE\t\t |  RUN TIME\t| PRICE\n");
+	printf("------------------------------------------\n");
+	for(int i = 0; i < amount; i++)
+	{
+		softWrap(input[i].title);
+		printf("%s\t\t%3i\t  %.2f\n\n", input[i].title, input[i].minutes, input[i].price);
+	}
+	printf("------------------------------------------\n");
+}
+
+
+// wraps the line after a certain number of characters
+// (in this case 15), by keeping track of the last Blank characters
+// and replacing it with a new line in case length of title is greater
+// than 15. It also adds spaces at the end of the title upto a certain length
+// so that the tab length is same for various title of varying length.
+void softWrap(char *title)
+{
+	int i, j = 1, nc = 0;
+	int lastBlank = 0;
+	int length = strlen(title);
+	char temp[30];
+
+	for(i = 0; i <= length; i++)
+	{
+		if(title[i] == ' ')
+		{
+			lastBlank = i;
+		}
+
+		// adds a new line at the last blank for every
+		// 15 intervals.
+		if(i >= 15 * j)
+		{
+			title[lastBlank] = '\n';
+			j++;
+		}
+	}
+
+	title[i] = '\0';
+	length = strlen(title);
+
+	// to count the length of the word in the last line of the title.
+	for(i = 0; i < length; i++)
+	{
+		if(title[i] == '\n')
+			nc = 0;
+
+		else nc++;
+	}
+
+	// adds spaces to the last line of the title.
+	for(j = 0; j < 15 - nc; j++)
+	{
+		temp[j] = ' ';
+	}
+
+	temp[j] = '\0';
+
+	strcat(title, temp);
+
 }
